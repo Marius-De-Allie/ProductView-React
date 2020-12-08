92041df773b76760ce92b8ce9d693fc941d1d3a8
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-08 13:58:11 -0500

    Combine redux reducers using combineReducers method

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'react-redux';
+import product from './product';
+
+// Combine redux reducers into single root reducer.
+export default combineReducers({
+  product
+});
