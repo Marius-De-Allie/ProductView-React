cb022acdb30ced67180003eae2fb49bd6ca67380
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-08 14:27:46 -0500

    Declare useEffect hook within App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProductDetails from './ProductDetails';
 import '../App.css';
 
 function App() {
+  // Fetch product details when App component is mounted.
+  useEffect(() => {
+    console.log('fetch product!');
+  }, []);
+
   return (
     <div className="App">
       App
