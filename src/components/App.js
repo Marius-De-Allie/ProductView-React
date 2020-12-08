7d609512b9868e295ecd2f8e49ab06e39da28435
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-08 14:23:09 -0500

    Create new ProductDetails component, which will render specific product details

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ProductDetails from './ProductDetails';
 import '../App.css';
 
 function App() {
   return (
     <div className="App">
       App
+      <ProductDetails />
     </div>
   );
 }
---
+++ src/components/ProductDetails.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProductDetails = () => {
+  
+  return (
+    <div className="prod-details">
+      <section classnalme="primary-details">
+        <div className="image">
+          <img src="" alt="" />
+        </div>
+        <p>price per unit</p>
+        <p>Total price</p>
+        <p>size</p>
+        <p>Image list placeHolder</p>
+      </section>
+      <section classnalme="sec-details">
+        description
+        reviews
+      </section>
+      Product Details Page
+    </div>
+  );
+};
+
+export default ProductDetails;
