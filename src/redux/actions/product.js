ca4db121db8ff6db5a6e774962a85125bf14f069
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-08 14:48:00 -0500

    Install redux-thunk library and declare handlefetchProd thunk action creator

--- src/redux/actions/product.js
+++ src/redux/actions/product.js
@@ -1,19 +1,43 @@
+// Import async API function to fetch products.
+import fetchProduct from '../../services/api';
+
 // Declare action types.
-const SUCCESS_FETCH_PRODS = 'SUCCESS_FETCH_PRODS';
-const FAIL_FETCH_PRODS = 'FAIL_FETCH_PRODS';
+const SUCCESS_FETCH_PROD = 'SUCCESS_FETCH_PROD';
+const FAIL_FETCH_PROD = 'FAIL_FETCH_PROD';
 
 // Successfully fetched product details action creator.
-const successFetchProds = (productObj) => ({
+const successFetchProd = (productObj) => ({
   type: SUCCESS_FETCH_PRODS,
   product: productObj
 });
 
 // Failue on product details fetch action creator.
-const failureFetchProds = (error) => ({
+const failureFetchProd = (error) => ({
   type: FAIL_FETCH_PRODS,
   error
 });
 
+// Thunk action creator.
+const handlefetchProd = () => {
+  return (dispatch) => {
+    // 1 .Make API call to fetch products details.
+    fetchProduct()
+    // If successful.
+    .then((product) => {
+      // 2. Dispatch successFetchProds action passing in the returned product.
+      dispatch(successFetchProd(product));
+    })
+    // if unsuccessful.
+    .catch(e => {
+      // 2. Dispatch failureFetchProds action passing in the error message.
+      dispatch(failureFetchProd(e.message));
+    });
+  }
+};
+
+
 export { 
-  SUCCESS_FETCH_PRODS 
+  SUCCESS_FETCH_PROD,
+  FAIL_FETCH_PROD,
+  handlefetchProd as default
 };
