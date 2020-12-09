a11f3be5931bbeba31d6947e20a01a9499a696e8
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-08 21:34:58 -0500

    Create Error reducer and ad dit to rootReducer

---
+++ src/redux/reducers/error.js
@@ -0,0 +1,11 @@
+
+// Error reducer.
+const error = (state = null, action) => {
+  switch(action.type) {
+
+    default:
+      return state;
+  }
+};
+
+export default error;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import product from './product';
 import cart from './cart';
+import error from './error';
 
 // Combine redux reducers into single root reducer.
 export default combineReducers({
   product,
-  cart
+  cart,
+  error
 });
