7b198506de96eedfde89c7c9e9e4ed2c71e904a6
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-08 21:37:41 -0500

    Add case for FAIL_FETCH_PROD to error reducer switch statement

--- src/redux/reducers/error.js
+++ src/redux/reducers/error.js
@@ -1,8 +1,9 @@
-
+import { FAIL_FETCH_PROD } from '../actions/product';
 // Error reducer.
 const error = (state = null, action) => {
   switch(action.type) {
-
+    case FAIL_FETCH_PROD:
+      return 'Unable to fetch product please try again.'
     default:
       return state;
   }
