b0a03a17d812eba30db9e9500eb86ec403901746
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-08 20:21:48 -0500

    Add ADD_TO_CART case to cart reducer switch statement

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,7 +1,13 @@
-//  cart reducer.
-const cart = (state = null, action) => {
-  switch(action.type) {
+import { ADD_TO_CART } from '../actions/cart';
 
+//  cart reducer.
+const cart = (state = [], action) => {
+  switch(action.type) {
+    case ADD_TO_CART:
+      return [
+        ...state,
+        cart
+      ];
     default:
       return state;
   }
